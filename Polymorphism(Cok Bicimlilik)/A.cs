3512d909c2d4623d6131f7305bdb70fc16e08533
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-10 03:02:11 +0300

    Initial commit

---
+++ Polymorphism(Cok Bicimlilik)/A.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Cok_Bicimlilik_
+{
+    public class A
+    {
+        public virtual string Method()
+        {
+            return "A içerik";
+        }
+    }
+
+    public class B : A
+    {
+        public override string Method()
+        {
+            return "B içerik";
+        }
+    }
+
+    public class C : A
+    {
+        public override string Method()
+        {
+            string str = base.Method();
+            return str + "C classından gelen içerik";
+        }
+    }
+}
+// Çok Biçimlilik: Base(temel, miras veren) sınıf içerisindeki üyelerin derived(türetilmiş)    sınıflar içerisinde farklı şekilde(class'a göre) davranmasını sağlama yaklaşımıdır.
+// C#'ta bir .cs uzantılı dosya class tutmak için vardır.
+// Virtual: Gövdelerinin derived class tarafından değiştirilebilir hale getirilmesi için       tanımlanan üyelerdir.
---
+++ Polymorphism(Cok Bicimlilik)/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polymorphism_Cok_Bicimlilik_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A a_nesnesi = new A();
+            MessageBox.Show(a_nesnesi.Method());
+
+            B b_nesnesi = new B();
+            MessageBox.Show(b_nesnesi.Method());
+
+            C c_nesnesi = new C();
+            MessageBox.Show(c_nesnesi.Method());
+        }
+    }
+}
